4847d25b078fdc229607e764641703e24192999e
rjindael <me@rjindael.xyz>
2023-07-31 01:26:44 -0700

    feat: initialize directory

--- Kiseki.Launcher.Windows/Program.cs
+++ Kiseki.Launcher.Windows/Program.cs
@@ -5,6 +5,9 @@ namespace Kiseki.Launcher.Windows
         [STAThread]
         static void Main(string[] args)
         {
+            string parentFolder = Path.GetDirectoryName(AppContext.BaseDirectory)!;
+            Directories.Initialize(parentFolder.ToLower().Contains(Launcher.ProjectName) ? AppContext.BaseDirectory : Path.Combine(Directories.LocalAppData, Launcher.ProjectName));
+
             ApplicationConfiguration.Initialize();
             Application.Run(new MainWindow(args));
         }
