1e0648a0050b49dad25c92e9e3b8afbba3ab4953
rjindael <me@rjindael.xyz>
2023-09-10 02:31:14 -0700

    feat: global usings among common Launcher project

---
+++ Kiseki.Launcher/Usings.cs
@@ -0,0 +1,6 @@
+global using System.Diagnostics;
+global using System.Text.Json.Serialization;
+global using System.Text.Json;
+
+global using Kiseki.Launcher.Enums;
+global using Kiseki.Launcher.Utilities;
