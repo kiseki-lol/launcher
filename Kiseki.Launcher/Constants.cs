3c604085f7f43102ce75b8e13caacdd90fc0513a
rjindael <me@rjindael.xyz>
2023-09-10 02:26:00 -0700

    feat: switch from kiseki.lol -> kiseki.loc on dbg

--- Kiseki.Launcher/Constants.cs
+++ Kiseki.Launcher/Constants.cs
@@ -5,7 +5,12 @@ public static class Constants
     public const string PROJECT_NAME = "Kiseki";
     public const string PROJECT_REPOSITORY = "kiseki-lol/launcher";
 
+#if DEBUG
+    public const string BASE_URL = "kiseki.loc";
+#else
     public const string BASE_URL = "kiseki.lol";
+#endif
+
     public const string MAINTENANCE_DOMAIN = "test";
 
     public const string PROTOCOL_KEY = "kiseki";
