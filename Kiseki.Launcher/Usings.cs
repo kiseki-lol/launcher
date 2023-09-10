bc55ce7e8392674a3101fab0447d4fde27f4f567
rjindael <me@rjindael.xyz>
2023-09-10 02:32:05 -0700

    fix: add Kiseki.Launcher.Models to Usings

--- Kiseki.Launcher/Usings.cs
+++ Kiseki.Launcher/Usings.cs
@@ -3,4 +3,5 @@ global using System.Text.Json.Serialization;
 global using System.Text.Json;
 
 global using Kiseki.Launcher.Enums;
+global using Kiseki.Launcher.Models;
 global using Kiseki.Launcher.Utilities;
